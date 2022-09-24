cb4cde436b114adabcfc073a89e8b7fc4caeefe7
Carlos Aguirre Rivera <carlosaguirre317@gmail.com>
2022-09-24 17:06:51 -0500

    fix: auth add Roles

--- src/auth/dto/register-auth.dto.ts
+++ src/auth/dto/register-auth.dto.ts
@@ -1,9 +1,13 @@
 import { PartialType } from "@nestjs/mapped-types";
-import { IsNumber, IsString,IsBoolean } from "class-validator";
+import { IsNumber, IsString,IsBoolean, IsArray } from "class-validator";
 import { LoginAuthDto } from "./login-auth.dto";
 
 export class RegisterAuthDto extends PartialType(LoginAuthDto){
     
     @IsString()
-    authFullname:   string    
+    authFullname:   string ;
+    @IsString()
+    authPassword:string;
+    @IsArray()
+    authRole:   string[];
 }
